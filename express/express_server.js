dbd31433639cc0ee74356e2de5808d97a4f7b57b
Anthony Langford <anthony.langford@gmail.com>
2017-01-18 14:11:44 +0000

    added homepage, header, changed html titles

--- express/express_server.js
+++ express/express_server.js
@@ -7,7 +7,7 @@ server.use(bodyParser.urlencoded({extended: true}));
 server.set("view engine", "ejs");
 server.use(express.static("public"));
 
-var urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -20,8 +20,8 @@ server.listen(8080, function() {
   console.log("Server started");
 })
 
-server.get("/", function(request, response) {
-  response.end("Hello");
+server.get("/", (request, response) => {
+  response.render("homepage");
 });
 
 server.get("/urls.json", (request, response) => {
@@ -38,7 +38,7 @@ server.get("/urls/new", (request, response) => {
 });
 
 server.post("/urls", (request, response) => {
-  var shortURL = generateRandomString();
+  let shortURL = generateRandomString();
   urlDatabase[shortURL] = request.body.longURL;
   response.redirect("/urls/" + shortURL);
 });
@@ -54,7 +54,16 @@ server.get("/u/:shortURL", (request, response) => {
   response.redirect(longURL);
 });
 
+server.post("/urls/:id/delete", (request, response) => {
+  delete urlDatabase[request.params.id];
+  response.redirect("/urls");
+});
 
+server.post("/urls/:id/update", (request, response) => {
+  delete urlDatabase[request.params.id];
+  urlDatabase[request.params.id] = request.body.newlongURL;
+  response.redirect("/urls");
+});
 
 
 // // create another route where you can input a person's name - anything after the / is set to 'name'
@@ -64,8 +73,3 @@ server.get("/u/:shortURL", (request, response) => {
 //                             showMoon: true})
 //   response.send(request.params.name)
 // })
-
-server.post("/", function(request, response) {
-  response.send("You made a post request");
-});
-
