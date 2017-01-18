a62e69d8bd7f44d01e6b2b994964939047917e8d
Anthony Langford <anthony.langford@gmail.com>
2017-01-18 21:42:01 +0000

    added 400 error handler functionalities to registration handler

--- express/express_server.js
+++ express/express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const server = express();
-
 const jsonfile = require('jsonfile');
+const debug = require('debug');
 // let db_path = './db.json';
 // const db = jsonfile.readFileSync(db_path);
 
@@ -27,7 +27,7 @@ server.use(express.static("public"));
 
 let users = {
   "userRandomID": {id: "userRandomID", email: "user@example.com", password: "unicorns"},
-  "user2RandomID": {id: "user2RandomID", email: "user2@example.com", password: "ligers"
+  "user2RandomID": {id: "user2RandomID", email: "user2@example.com", password: "ligers"}
 }
 
 let urlDatabase = {
@@ -51,28 +51,39 @@ server.get("/", (request, response) => {
 server.get("/login", (request, response) => {
   let templateVars = {username: request.cookies["username"]};
   response.render("login", templateVars);
-})
+});
 
 server.post("/login", (request, response) => {
   response.cookie("username", request.body.username, {maxAge: 864000});
   response.redirect("/");
-})
+});
 
 server.get("/register", (request, response) => {
-  let templateVars = {username: request.cookies["username"]};
+  let templateVars = {userList: users};
   response.render("register", templateVars);
-})
+});
 
-server.post("/register", (rewuest, response) => {
-  users[username] = {request.body.id, request.body.email, request.body.password};
-  response.cookie("username", request.body.username, {maxAge: 864000});
-  response.render("register");
-})
+server.post("/register", (request, response) => {
+  if (!request.body.email || !request.body.password) {
+    response.status(400).send('Sorry! You need to provide both an email and a password.');
+    return;
+  }
+  for (let user in users) {
+    if (users[user].email === request.body.email) {
+      response.status(400).send('Sorry! That email address has already been registered. If you think this is a mistake, please contact us somehow.');
+      return;
+    };
+  };
+  let userid = generateRandomString();
+  users[userid] = {id: userid, email: request.body.email, password: request.body.password};
+  response.cookie(userid, request.body.username, {maxAge: 864000});
+  response.redirect("/");
+});
 
 server.post("/logout", (request, response) => {
   response.clearCookie("username");
   response.redirect("/");
-})
+});
 
 server.get("/urls", (request, response) => {
   let templateVars = {urls: urlDatabase,
@@ -118,6 +129,12 @@ server.post("/urls/:id/update", (request, response) => {
   urlDatabase[request.params.id] = request.body.newLongURL;
   response.redirect("/urls");
 });
+
+
+
+
+
+
 
 // middleware, just like get and post. get and post also technically receive next but they respond to the server so they don't need to pass it on
 // server.use(function(request, response, next) {
